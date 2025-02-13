110b9441dbbc5c51c5128b42b85012f86aeefd5a
pythonstrup <pythonstrup@gmail.com>
2025-02-13 21:50:07 +0900

    feat: 화면 고정

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:jaknaeso_flutter/config/env_config.dart';
 import 'package:jaknaeso_flutter/screen/webview_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EnvConfig.loadEnv();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(
     MaterialApp(
