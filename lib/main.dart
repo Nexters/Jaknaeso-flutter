f66fc34757173f85d96a45045d8f9d37a7f53fa9
pythonstrup <pythonstrup@gmail.com>
2025-02-19 21:15:35 +0900

    feat: SplashScreen 추가 (#25)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:jaknaeso_flutter/config/env_config.dart';
 import 'package:jaknaeso_flutter/screen/webview_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
   await EnvConfig.loadEnv();
-
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(
@@ -14,4 +15,8 @@ void main() async {
       home: WebViewScreen(),
     ),
   );
+
+  Future.delayed(Duration(seconds: 2), () {
+    FlutterNativeSplash.remove();
+  });
 }
